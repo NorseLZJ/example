6db43e36938a3a5699d3516984cbb945c8540d05
lzj <18841685054@163.com>
2020-03-21 19:44:40 +0800

    ch

--- fileSrv/build.sh
+++ fileSrv/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 if [ "$1" = 'windows' ]; then
-export GOOS=linux 
+export GOOS=windows
 export GOARCH=amd64
+export GOHOSTARCH=amd64
+export GOHOSTOS=windows
+export GOEXE=.exe
 go build -ldflags='-w -s' -o $GOPATH/src/github.com/NorseLZJ/program/fileServer/fileServer.exe 
 else 
 go build -ldflags='-w -s' -o $GOPATH/src/github.com/NorseLZJ/program/fileServer/fileServer 
