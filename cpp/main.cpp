f4ff472c4afa8994243fbceb4d66f75a7bd8543a
lzj <18841685054@163.com>
2020-04-23 15:53:43 +0800

    ch

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <assert.h>
 #include "src/people.h"
 #include "src/calc.h"
-#include "src/m_mysql.h"
+#include "src/mysql_connection_pool.h"
 
-int main()
+int main(int argc, char **argv)
 {
   // people
   //People p("lzj",11);
@@ -26,20 +27,53 @@ int main()
   //c.print();
 
   // mysql
-  M_Mysql sql;
-  int ret = sql.initMysqlConnPool("192.168.20.177", 6380, "lzj", "123456", "cpp_test");
-  if (ret == INIT_SUCCESS)
+  char str[] = "CREATE TABLE `ban` ( \
+  `userId` bigint NOT NULL COMMENT 'id', \
+  `openId` varchar(60) NOT NULL DEFAULT '' COMMENT 'openId', \
+  PRIMARY KEY (`userId`) \
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+  //char str[] = "CREATE TABLE `ban` ( \
+  //`userId` bigint NOT NULL COMMENT 'id', \
+  //`openId` varchar(60) NOT NULL DEFAULT '' COMMENT 'openId', \
+  //PRIMARY KEY (`userId`) \
+  //) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+
+  MysqlConnectionPool *pool = new MysqlConnectionPool();
+  int ret = 0;
+  ret = pool->initMysqlConnPool("127.0.0.1", 3307, "root", "123456", "cpp_test");
+  assert(ret == 0);
+  ret = pool->openConnPool(10);
+  assert(ret == 0);
+  int num = 1;
+  MYSQL_RES *res_ptr;
+  int i, j;
+  MYSQL_ROW sqlrow;
+  while (num > 0)
   {
-    printf("connect success ...\n");
+  mysqlConnection *mysqlConn = pool->fetchConnection();
+  assert(mysqlConn != NULL);
+  pool->executeSql(mysqlConn, str);
+  res_ptr = mysql_store_result(mysqlConn->sock);
+  if (res_ptr)
+    {
+      printf("%lu Rows\n", (unsigned long)mysql_num_rows(res_ptr));
+      j = mysql_num_fields(res_ptr);
+      while ((sqlrow = mysql_fetch_row(res_ptr)))
+      {
+        for (i = 0; i < j; i++)
+          printf("%s\t", sqlrow[i]);
+        printf("\n");
+      }
+      if (mysql_errno(mysqlConn->sock))
+      {
+        fprintf(stderr, "Retrive error:%s\n", mysql_error(mysqlConn->sock));
+      }
+    }
+    mysql_free_result(res_ptr);
+    pool->recycleConnection(mysqlConn);
+    num--;
   }
-  mysqlConn *conn = sql.fetchConnection();
-  char tsql[] = "CREATE TABLE ts_cpp;";
-  const char *psql = tsql;
-  ret = sql.executeSql(conn, psql);
-  if (ret == INIT_SUCCESS)
-  {
-    printf("exec success ...\n");
-  }
+  delete pool;
 
   return 0;
 }
