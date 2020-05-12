64cb82388aad122aba590bee762865c0ad5c7c8f
lzj <liuzijian@dongmengwl.com>
2020-05-12 22:08:08 +0800

    ch

--- cmd/reptile/main.go
+++ cmd/reptile/main.go
@@ -1,37 +1,63 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"net/http"
+	"bufio"
+	"fmt"
+	"io"
 
-	log2 "github.com/NorseLZJ/example/std/log"
+	"github.com/gpmgo/gopm/modules/log"
+
+	"github.com/gocolly/colly"
+
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
 )
 
 func main() {
-	llog := log2.LLog()
-	url := "https://sh.lianjia.com/ershoufang/"
-	resp, err := http.Get(url)
+	//llog := log2.LLog()
+	//resp, err := http.Get("http://www.zhenai.com/zhenghun") //获取页面返回的response
+	url := "https://xa.lianjia.com/ershoufang/"
+	//resp, err := http.Get(url)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//defer resp.Body.Close()
+	//if resp.StatusCode != http.StatusOK {
+	//	log.Fatal("resp.StatusCode :%d", resp.StatusCode)
+	//}
+	//utf8Reader := transform.NewReader(resp.Body, coder(resp.Body).NewDecoder())
+	//all, err := ioutil.ReadAll(utf8Reader)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fd, err := os.OpenFile("tmp.html", os.O_WRONLY|os.O_TRUNC, 0600)
+	//if err != nil {
+	//	panic(err)
+	//}
+	////fmt.Printf("%s", all)
+	//defer fd.Close()
+	//fd.Write(all)
+
+	// colly
+	c := colly.NewCollector()
+	// Find and visit all links
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		log.Info("%s", e.Name)
+		e.Request.Visit(e.Attr("href"))
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.Visit(url)
+}
+
+func coder(r io.Reader) encoding.Encoding {
+	bytes, err := bufio.NewReader(r).Peek(1024)
 	if err != nil {
-		llog.Error("%v", err)
+		panic(err)
 	}
-
-	respBody, err := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
-	if err != nil {
-		llog.Error("%v", err)
-	}
-
-	if resp.StatusCode != 200 {
-		llog.Error("status code err %d", resp.StatusCode)
-	}
-
-	if len(respBody) == 0 {
-		respBody = []byte("{}")
-	}
-
-	var wResp interface{}
-	if err = json.Unmarshal(respBody, &wResp); err != nil {
-		llog.Error("Unmarshal err ", err)
-	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
