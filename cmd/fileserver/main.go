b053ffffcf86349f587a041ef79262398405419b
lzj <liuzijian@dongmengwl.com>
2020-07-09 17:34:27 +0800

    fsv

--- cmd/fileserver/main.go
+++ cmd/fileserver/main.go
@@ -3,80 +3,67 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
-	"os"
+	"runtime"
 
 	"github.com/NorseLZJ/example/std"
 )
 
 var (
-	port   = flag.String("p", ":18000", "server port")
-	share  = flag.String("s", "", "public dir")
-	upload = flag.String("u", "", "upload dir")
+	port = flag.String("p", "8900", "server port")
+	help = flag.Bool("h", false, "help ")
 )
+
+const (
+	shareWindows = "D:\\share\\"
+	shareLinux   = "~/share/"
+	windows      = `windows`
+	linux        = `linux`
+)
+
+var cc = `
+you need create share dir before run fsv.exe 
+
+usage: fsv [-p]
+Example:
+1、fsv 
+2、fsv -p 9000 
+
+Default:
+Windows: share dir 	"D:\share\"
+Linux: share dir 	"~/share/"
+`
 
 func main() {
 	flag.Parse()
-	if *share == "" {
+	flag.Usage = func() {
+		fmt.Printf("%s\n", cc)
+	}
+
+	if *help {
+		flag.Usage()
+		return
+	}
+	share := ""
+	switch runtime.GOOS {
+	case windows:
+		share = shareWindows
+	case linux:
+		share = shareLinux
+	default:
 		log.Fatal("share dir is nil")
 	}
+
 	fmt.Println("start server")
+	*port = fmt.Sprintf(":%s", *port)
 	showIp(*port)
 	//http.HandleFunc("/", uploadFileHandler)
 	//http.Handle("/file", http.StripPrefix("/file", http.FileServer(http.Dir(cfgT.ShareDir))))
-	http.ListenAndServe(*port, http.FileServer(http.Dir(*share)))
-}
-
-func uploadFileHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>文件互传</title>
-</head>
-<body style="text-align: center;"> 
-    <h1>文件互传</h1>
-    <br>
-    <br>
-    <form action="UploadFile.ashx" method="post" enctype="multipart/form-data">
-    <input type="file" name="fileUpload" />
-    <input type="submit" name="上传文件">
-    </form>
-        <br>
-    <br>
-        <br>
-    <br>
-    <a href="/file">文件下载</a>
-</body>
-</html>
-        `)
-	if r.Method == "POST" {
-		file, handler, err := r.FormFile("fileUpload") //name的字段
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		defer file.Close()
-		fileBytes, err := ioutil.ReadAll(file)
-		if err != nil {
-			log.Printf("err : %v", err)
-			return
-		}
-		newFile, err := os.Create(*upload + handler.Filename)
-		if err != nil {
-			log.Printf("err : %v", err)
-			return
-		}
-		defer newFile.Close()
-		if _, err := newFile.Write(fileBytes); err != nil {
-			std.CheckErr(err)
-		}
-		fmt.Println("upload successfully:" + *upload + handler.Filename)
-		w.Write([]byte("SUCCESS"))
+	err := http.ListenAndServe(*port, http.FileServer(http.Dir(share)))
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
@@ -95,3 +82,53 @@ func showIp(port string) {
 		fmt.Println(fmt.Sprintf("%s%s", v, port))
 	}
 }
+
+//func uploadFileHandler(w http.ResponseWriter, r *http.Request) {
+//	fmt.Fprintln(w, `
+//<!DOCTYPE html>
+//<html lang="en">
+//<head>
+//    <meta charset="UTF-8">
+//    <title>文件互传</title>
+//</head>
+//<body style="text-align: center;">
+//    <h1>文件互传</h1>
+//    <br>
+//    <br>
+//    <form action="UploadFile.ashx" method="post" enctype="multipart/form-data">
+//    <input type="file" name="fileUpload" />
+//    <input type="submit" name="上传文件">
+//    </form>
+//        <br>
+//    <br>
+//        <br>
+//    <br>
+//    <a href="/file">文件下载</a>
+//</body>
+//</html>
+//        `)
+//	if r.Method == "POST" {
+//		file, handler, err := r.FormFile("fileUpload") //name的字段
+//		if err != nil {
+//			fmt.Println(err)
+//			return
+//		}
+//		defer file.Close()
+//		fileBytes, err := ioutil.ReadAll(file)
+//		if err != nil {
+//			log.Printf("err : %v", err)
+//			return
+//		}
+//		newFile, err := os.Create(*upload + handler.Filename)
+//		if err != nil {
+//			log.Printf("err : %v", err)
+//			return
+//		}
+//		defer newFile.Close()
+//		if _, err := newFile.Write(fileBytes); err != nil {
+//			std.CheckErr(err)
+//		}
+//		fmt.Println("upload successfully:" + *upload + handler.Filename)
+//		w.Write([]byte("SUCCESS"))
+//	}
+//}
