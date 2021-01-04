158334413eaf630b8c2c60cfe4302340d5ee95d9
lzj <18841685054@163.com>
2021-01-04 21:57:03 +0800

    ccrontab 50%

--- cmd/ccrontab/main.go
+++ cmd/ccrontab/main.go
@@ -1,15 +1,26 @@
 package main
 
-import "flag"
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/robfig/cron"
+)
 
 type Task struct {
-	Cmd    string `json:"cmd"`
-	Script string `json:"script"`
-	Space  string `json:"space"`
+	Cmd    string `json:"cmd"`    // bin/bash
+	Script string `json:"script"` // /aaa/bbb.sh
+	Space  string `json:"space"`  // * * * * * *
 }
 
 type Config struct {
-	LogDir string `json:"log_dir"`
+	LogDir string `json:"logDir"`
 	Tasks  []Task `json:"tasks"`
 }
 
@@ -19,5 +30,33 @@ var (
 
 func main() {
 	flag.Parse()
+	cfgT := &Config{}
+	data, err := ioutil.ReadFile(*cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = json.Unmarshal(data, cfgT)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	c := cron.New()
+	c.AddFunc("* * * * *", func() { fmt.Println("Every hour on the half hour") })
+	c.Start()
+	waitExit()
+}
+
+func waitExit() {
+	sigS := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	signal.Notify(sigS, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		sig := <-sigS
+		fmt.Println()
+		fmt.Println(sig)
+		done <- true
+	}()
+	fmt.Println("ccrontab start ...,and waiting some code")
+	<-done
+	fmt.Println("exiting")
 }
