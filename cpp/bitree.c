04b947310cdbbbadaff43402b2f4867280b75049
NorseLZJ <18841685054@163.com>
2021-01-20 23:59:53 +0800

    bitree done

--- cpp/bitree.c
+++ cpp/bitree.c
@@ -1,4 +1,6 @@
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/malloc.h>
 
 typedef struct Node
@@ -18,23 +20,21 @@ int main(int argc, char **argv)
     int n = sizeof(table) / sizeof(table[0]);
     int x, i;
     for (i = 0; i < n; i++)
-    {
         BSTInsert(&T, table[i]);
-    }
+    
     printf("\n---------\n");
-    InOrderTraverse(T);
-    printf("\ninput search num:");
-    scanf("%d", &x);
-    p = BSTSearch(T, x);
-    if (p != NULL)
-    {
-        printf("\nfind it's :%d", x);
+    InOrderTraveres(T);
+    for(;;){
+        
+        printf("\ninput search num:");
+        scanf("%d", &x);
+        p = BSTSearch(T, x);
+        if (p != NULL)
+            printf("\nfind it's :%d", x);
+        else
+            printf("\ncan't find it's :%d", x);
     }
-    else
-    {
-        printf("\ncan't find it's :%d", x);
-    }
-
+    
     return 0;
 }
 
@@ -47,17 +47,11 @@ BiTree BSTSearch(BiTree T, int x)
         while (p != NULL)
         {
             if (p->data == x)
-            {
                 return p;
-            }
             else if (x < p->data)
-            {
                 p = p->lchild;
-            }
             else
-            {
                 p = p->rchild;
-            }
         }
     }
     return NULL;
@@ -70,45 +64,37 @@ int BSTInsert(BiTree *T, int x)
     while (cur != NULL)
     {
         if (cur->data == x)
-        {
             return 0;
-        }
+        
         parent = cur;
-        if (x < cur->lchild)
-        {
+        if (x < cur->data)
             cur = cur->lchild;
-        }
         else
-        {
             cur = cur->rchild;
-        }
     }
     p = (BiTreeNode *)malloc(sizeof(BiTreeNode));
     if (!p)
-    {
-        return 0;
-    }
+        exit(1);
+    
     p->data = x;
     p->lchild = NULL;
     p->rchild = NULL;
     if (!parent)
-    {
-        *T = parent;
-    }
+        *T = p;
     else if (x < parent->data)
-    {
         parent->lchild = p;
-    }
     else
-    {
         parent->rchild = p;
-    }
     return 1;
 }
 
 void InOrderTraveres(BiTree T)
 {
+    if (!T)
+        return;
+    
     InOrderTraveres(T->lchild);
     printf("%4d", T->data);
     InOrderTraveres(T->rchild);
 }
+
