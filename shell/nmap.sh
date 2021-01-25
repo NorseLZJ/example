c25c9e93802076da66b7b82efaefc5b7be5c4819
NorseLZJ <18841685054@163.com>
2021-01-25 23:52:09 +0800

    change nmap.sh

--- shell/nmap.sh
+++ shell/nmap.sh
@@ -1,21 +1,22 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 ip=$1
 
 split_str="*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-"
 
-if [ -n "$ip" ]; then
-    cat /dev/null >nmap_report.txt
-    lst=$(nmap -sP ${ip}'/24' | grep 'Nmap scan report for' | awk '{print $5}')
-
-    for i in ${lst}; do
-        #echo ${i}
-        printf ${i}'\n' >>nmap_report.txt
-
-        report="nmap_report.txt"
-        $(nmap -F -T5 --version-light --top-ports 300 ${i} >>${report})
-        echo ${split_str} >>nmap_report.txt
-    done
-else
+if [ -z "$ip" ]; then
     echo "param err"
+    exit
 fi
+
+cat /dev/null >nmap_report.txt
+lst=$(nmap -sP ${ip}'/24' | grep 'Nmap scan report for' | awk '{print $5}')
+
+for i in ${lst}; do
+    #echo ${i}
+    printf ${i}'\n' >>nmap_report.txt
+
+    report="nmap_report.txt"
+    $(nmap -F -T5 --version-light --top-ports 300 ${i} >>${report})
+    echo ${split_str} >>nmap_report.txt
+done
