f9ddcb364af40038704a2c55f4bc0230346f9421
NorseLZJ <18841685054@163.com>
2021-01-26 00:21:51 +0800

    nmap 多线程‘

--- shell/nmap.sh
+++ shell/nmap.sh
@@ -13,10 +13,11 @@ cat /dev/null >nmap_report.txt
 lst=$(nmap -sP ${ip}'/24' | grep 'Nmap scan report for' | awk '{print $5}')
 
 for i in ${lst}; do
-    #echo ${i}
-    printf ${i}'\n' >>nmap_report.txt
-
-    report="nmap_report.txt"
-    $(nmap -F -T5 --version-light --top-ports 300 ${i} >>${report})
-    echo ${split_str} >>nmap_report.txt
+    {
+        report="nmap_report.txt"
+        $(nmap -F -T5 --version-light --top-ports 300 ${i} >>${report})
+        echo ${split_str} >>nmap_report.txt
+    } &
 done
+wait
+echo "check done."
