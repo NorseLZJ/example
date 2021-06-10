2d8a7ea213cde6c826a0d7be8f48239d138429b4
Redington <liujinjia@testerlife.com>
2021-06-10 14:38:09 +0800

    nmap shell

---
+++ shell/jenkins.sh
@@ -0,0 +1,8 @@
+#!/bin/env bash
+
+cmd=$1
+if [[ $cmd != "" &&  $cmd == "scan" ]];then
+    nmap -p 8080 -oX jenkins.xml 192.168.1.1-251
+fi
+
+grep -n -B 4 'open' jenkins.xml | grep 'address addr=' | grep -v 'mac' | awk '{print $2}'
