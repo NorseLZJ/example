e8a6b9144d2475945aa3c9b8483ca61518474649
NorseLZJ <18841685054@163.com>
2021-07-30 05:09:40 +0000

    redis conn success

--- go/redix/go.mod
+++ go/redix/go.mod
@@ -1,4 +1,4 @@
-module redis
+module redix
 
 go 1.16
 
--- go/redix/main.go
+++ go/redix/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"time"
+
 	"github.com/davyxu/cellnet"
 	"github.com/davyxu/cellnet/peer"
 	"github.com/davyxu/golog"
+	"github.com/mediocregopher/radix.v2/redis"
 
 	_ "github.com/davyxu/cellnet/peer/redix"
 	_ "github.com/davyxu/cellnet/proc/tcp"
@@ -16,19 +19,39 @@ func main() {
 	queue := cellnet.NewEventQueue()
 	p := peer.NewGenericPeer("redix.Connector", "server", "127.0.0.1:6379", queue)
 
-	//proc.BindProcessorHandler(p, "tcp.ltv", func(ev cellnet.Event) {
-	//	switch msg := ev.Message().(type) {
-	//	case *cellnet.SessionAccepted:
-	//		log.Debugln("server accepted")
-	//	case *cellnet.SessionClosed:
-	//		log.Debugln("session closed: ", ev.Session().ID())
-	//	default:
-	//		_ = msg
-	//		fmt.Printf("unknow msg")
-	//	}
-	//})
+	conn, ok := p.(cellnet.RedisConnector)
+	if !ok {
+		panic("p is not redisConnector")
+	}
+	conn.SetConnectionCount(1)
+	conn.SetDBIndex(2)
 
 	p.Start()
+
+	time.AfterFunc(time.Second*2, func() {
+		go func() {
+			for {
+				time.Sleep(time.Second)
+				opt, ok := p.(cellnet.RedisPoolOperator)
+				if !ok {
+					panic("opt is not RedisPoolOperator")
+				}
+				opt.Operate(func(c interface{}) (ret interface{}) {
+					if c == nil {
+						log.Infof("redis client is nil")
+						return nil
+					}
+					exec, ok := c.(*redis.Client)
+					if !ok {
+						log.Infof("client is not redis.Client")
+						return nil
+					}
+					exec.Cmd("SET", "a", "b")
+					return nil
+				})
+			}
+		}()
+	})
 	queue.StartLoop()
 	queue.Wait()
 }
