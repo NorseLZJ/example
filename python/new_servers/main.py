43e177832ceec36eee08a48606a984ad6989419d
liuzijian <liuzijian@3975.com>
2021-09-07 13:22:18 +0800

    可以手动指定固定目录

--- python/new_servers/main.py
+++ python/new_servers/main.py
@@ -29,14 +29,23 @@ g_path = os.getcwd()  # 全局路径,从这里开始
 
 
 def start_and_dir():
+    d = ""
+    with open('config.json', 'r') as f:
+        data = f.read()
+        val = json.loads(data)
+        d = val['srvnay']
+    if d != "":
+        global g_path
+        g_path = d
+
     s1 = g_path.split(':')[0] + ":"
     c_dir1 = g_path.split(':')[1]
     c_dir1 = format("%s%s" % (s1, c_dir1))
     return s1, c_dir1
 
 
-if __name__ == '__main__':
-    start, d = start_and_dir()
+def start():
+    s1, d = start_and_dir()
     with open('config.json', 'r') as f:
         data = f.read()
         val = json.loads(data)
@@ -45,8 +54,9 @@ if __name__ == '__main__':
             c_name = v['name']
             c_dir = v['dir']
             c_exe = v['exe']
+
             # 创建服务
-            cmd_1 = format(cd % (start, d, c_name, d))
+            cmd_1 = format(cd % (s1, d, c_name, d))
             print(cmd_1)
             os.system(cmd_1)
 
@@ -61,3 +71,7 @@ if __name__ == '__main__':
             cmd_4 = format(add_exe % (c_name, c_exe))
             print(cmd_4)
             os.system(cmd_4)
+
+
+if __name__ == '__main__':
+    start()
