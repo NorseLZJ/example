d0d4a96e7dca92054f7917c4235a6f1885ef1c0e
Redington <liujinjia@testerlife.com>
2021-12-23 22:40:28 +0800

    some code

---
+++ shell/inter.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+nmap -sP 192.168.199.1/24 -oX inter.xml 
+
+nmap -sP 192.168.199.1/24 -oX inter2.xml 
+
---
+++ shell/wifi.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+nmap -sP 192.168.88.1/24 -oX wifi.xml 
+
+
