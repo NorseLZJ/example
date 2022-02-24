a7a9e849a636c8c73b3b512b56d4cdd40de6004a
Redington <liujinjia@testerlife.com>
2022-02-24 23:37:18 +0800

    rust actix-web demo

--- Rust/actix-web-test/src/main.rs
+++ Rust/actix-web-test/src/main.rs
@@ -1,9 +1,34 @@
 use actix_web::{middleware, web, App, HttpRequest, HttpServer, Responder};
 use serde_derive::Deserialize;
+use std::fs;
 
 #[derive(Deserialize)]
 pub struct ReqParam {
     group: String,
+}
+
+fn dir_exist(group: &str) -> (bool, String, usize) {
+    let c_path = std::env::current_dir().unwrap();
+    for entry in fs::read_dir(c_path).unwrap() {
+        let entry = entry.unwrap();
+        let path = entry.path().display().to_string();
+        let md = std::fs::metadata(&path).unwrap();
+        if md.is_file() {
+            continue;
+        }
+
+        let idx = path.find(group);
+        match idx {
+            Some(idx) => {
+                println!("path:{} idx: {}", path, idx);
+                return (true, path.clone(), idx);
+            }
+            None => {
+                //println!("None...");
+            }
+        }
+    }
+    (false, String::from(""), 0)
 }
 
 async fn index(req: HttpRequest) -> &'static str {
@@ -12,7 +37,11 @@ async fn index(req: HttpRequest) -> &'static str {
 }
 
 async fn stop(web::Query(info): web::Query<ReqParam>) -> impl Responder {
-    format!("Stop Success {}!", info.group)
+    let (ret, path, _) = dir_exist(&info.group);
+    if ret {
+        return format!("Stop Success {}!", path);
+    }
+    format!("Unknow group {}!", info.group)
 }
 
 async fn start(web::Query(info): web::Query<ReqParam>) -> impl Responder {
