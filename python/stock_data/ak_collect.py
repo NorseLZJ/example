0ac75d84f3739897a250af45fd877c39781e5bec
lzj <18841685054@163.com>
2022-06-25 11:20:10 +0800

    modify

--- python/stock_data/ak_collect.py
+++ python/stock_data/ak_collect.py
@@ -1,6 +1,6 @@
 import talib
 import pandas as pd
-import numpy
+import numpy as np
 import mplfinance as mpf
 import akshare as ak
 
@@ -11,16 +11,18 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 500)
 
 
-def is_jc(_dif, _dea):  # 金叉
+def is_jc(_dif, _dea, _close, _low):  # 金叉
     if _dif >= _dea:
-        return 100
-    return 0
+        #return _close * 0.95 * _low
+        return _close * 0.95
+    return np.nan
 
 
-def is_sc(_dif, _dea):  # 金叉
+def is_sc(_dif, _dea, _close, _low):  # 金叉
     if _dif >= _dea:
-        return 0
-    return 100
+        return np.nan
+    #return _close * 0.95 * _low
+    return _close * 1.15
 
 
 def get_params(_df, idx):
@@ -45,13 +47,13 @@ def get_params(_df, idx):
 def collect_data(_data):
     _df = pd.read_json(_data)
     (dif, dea, macd) = talib.MACD(_df['close'], fastperiod=short_win, slowperiod=long_win, signalperiod=macd_win)
-    ma5 = numpy.around(talib.SMA(_df['close'], timeperiod=5), 2)
-    ma10 = numpy.around(talib.SMA(_df['close'], timeperiod=10), 2)
-    ma20 = numpy.around(talib.SMA(_df['close'], timeperiod=20), 2)
-    ma60 = numpy.around(talib.SMA(_df['close'], timeperiod=60), 2)
-    dif = numpy.around(dif, 2)
-    dea = numpy.around(dea, 2)
-    macd = numpy.around(macd, 2)
+    ma5 = np.around(talib.SMA(_df['close'], timeperiod=5), 2)
+    ma10 = np.around(talib.SMA(_df['close'], timeperiod=10), 2)
+    ma20 = np.around(talib.SMA(_df['close'], timeperiod=20), 2)
+    ma60 = np.around(talib.SMA(_df['close'], timeperiod=60), 2)
+    dif = np.around(dif, 2)
+    dea = np.around(dea, 2)
+    macd = np.around(macd, 2)
 
     _df.insert(loc=5, column='dif', value=dif)
     _df.insert(loc=5, column='dea', value=dea)
@@ -61,9 +63,8 @@ def collect_data(_data):
     _df.insert(loc=5, column='ma20', value=ma20)
     _df.insert(loc=5, column='ma60', value=ma60)
 
-    _df['signal_long'] = _df.apply(lambda x: is_jc(x['dif'], x['dea']), axis=1)
-    _df['signal_short'] = _df.apply(lambda x: is_sc(x['dif'], x['dea']), axis=1)
-    df.dropna(axis=1, inplace=True)
+    _df.dropna(axis=0, inplace=True)
+    _df.reset_index(inplace=True)
     return _df
 
 
@@ -71,27 +72,36 @@ if __name__ == "__main__":
     df = ak.stock_zh_index_daily_em(symbol='sz002424')
     df = collect_data(df.to_json())
     # (date, _, _, _, close, dif, dea, macd, ma5, ma10, ma20, ma60) = get_params(df, len(df) - 1)
+    #print(df.head(3))
+    #print(df.tail(3))
     x = get_params(df, len(df) - 1)
-    # df = df.iloc[len(df) - 200:len(df) - 1]
+    df = df.iloc[len(df) - 200:len(df) - 1]
     # print(x)
     # print(df.tail(10))
     df.to_excel('opt.xlsx', index=False, startrow=3, startcol=1)
 
     my_color = mpf.make_marketcolors(up='red', down='green', edge='inherit', volume='inherit')
     my_stype = mpf.make_mpf_style(marketcolors=my_color)
-    print(df.head(3))
 
     datetime_series = pd.to_datetime(df['date'])
     datetime_index = pd.DatetimeIndex(datetime_series.values)
 
+    buy = df.apply(lambda x: is_jc(x['dif'], x['dea'], x['close'], x['low']), axis=1)
+    sellout = df.apply(lambda x: is_sc(x['dif'], x['dea'], x['close'], x['low']), axis=1)
+
+    #buy = np.where((df['close'] > df['open']) & (df['close'].shift(1) < df['open'].shift(1)), 1, np.nan) * 0.95 * df['low']
+
     add_plot = [
         mpf.make_addplot(df[['ma5', 'ma10', 'ma20', 'ma60']]),
-        # mpf.make_addplot(df['signal_long'], scatter=True, markersize=80, marker='^', color='r'),
-        # mpf.make_addplot(df['signal_short'], scatter=True, markersize=80, marker='s', color='g')
+        #mpf.make_addplot(df['signal_long'], scatter=True, markersize=5, marker="^", color='r'),
+        #mpf.make_addplot(df['signal_short'], scatter=True, markersize=5, marker="s", color='g')
+        #mpf.make_addplot(buy, scatter=True, markersize=50, marker=r'$\Uparrow$', color='green')
+        mpf.make_addplot(buy, scatter=True, markersize=50, marker=r'$\Uparrow$', color='red'),
+        mpf.make_addplot(sellout, scatter=True, markersize=50, marker=r'$\Downarrow$', color='green')
     ]
 
     df2 = df.set_index(datetime_index)
     # df['date'].astype(pd.DatetimeIndex)
-    print(df2.info())
+    #print(df2.info())
     mpf.plot(df2, type='candle', ylabel='price', style=my_stype, addplot=add_plot, volume=True, ylabel_lower='vol')
     # mpf.plot(df2, type='line', ylabel='price', style=my_stype)
