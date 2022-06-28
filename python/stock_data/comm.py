b789a548fc7dc43ddbbab2699fc4ddfcea3fa565
liuzijian <liuzijian@3975.com>
2022-06-28 12:58:23 +0800

    modify

--- python/stock_data/comm.py
+++ python/stock_data/comm.py
@@ -13,13 +13,18 @@ pd.set_option("display.width", 500)
 
 
 def get_industry(_symbol: str):
-    vv = ak.stock_individual_info_em(_symbol)
-    item = vv['item']
-    value = vv['value']
+    _symbol = str(_symbol)
+    try:
+        vv = ak.stock_individual_info_em(_symbol)
+    except Exception as e:
+        print("get industry(%s) err:%s" % (_symbol, e))
+        return np.nan
+    item = vv["item"]
+    value = vv["value"]
     for i in range(len(item)):
-        if item[i] == '行业':
+        if item[i] == "行业":
             return value[i]
-    return "empty"
+    return np.nan
 
 
 def day_60_plus(x):
@@ -49,19 +54,18 @@ def get_name_akshare(code: str):
 
 def get_daily_data(_symbol: str):
     td = dt.date.today()
-    end_date = str(td).replace('-', '')
+    end_date = str(td).replace("-", "")
     timestamp = datetime.timestamp(datetime.now())
     dt_object = datetime.fromtimestamp(int(timestamp) - 356 * 86400)
-    start_date = (str(dt_object).split(' ')[0]).replace(' ', '')
+    start_date = (str(dt_object).split(" ")[0]).replace(" ", "")
     name = get_name_akshare(_symbol)
-    if name == '':
+    if name == "":
         return None
     try:
         # 拿一年左右前复权的数据
-        df = ak.stock_zh_a_daily(name,
-                                 start_date=start_date,
-                                 end_date=end_date,
-                                 adjust='qfq')
+        df = ak.stock_zh_a_daily(
+            name, start_date=start_date, end_date=end_date, adjust="qfq"
+        )
         return df
     except Exception as e:
         print("get stock daily data[%s] err:%s" % (_symbol, e))
@@ -69,13 +73,13 @@ def get_daily_data(_symbol: str):
 
 
 def get_minute_data(_symbol: str, period: str):
-    if period not in ('1', '5', '15', '30', '60'):
+    if period not in ("1", "5", "15", "30", "60"):
         return None
     name = get_name_akshare(_symbol)
-    if name == '':
+    if name == "":
         return None
     try:
-        df = ak.stock_zh_a_minute(name, period=period, adjust='qfq')
+        df = ak.stock_zh_a_minute(name, period=period, adjust="qfq")
         return df
     except Exception as e:
         print("get stock minute(%s) data[%s] err:%s" % (period, _symbol, e))
@@ -87,7 +91,7 @@ def get_params(_df, idx):
     params
         _df pandas.DataFrame
         idx _df index
-    return (date,open,high,low,close,dif,dea,macd,ma5,ma10,ma20,ma60,ma120) 
+    return (date,open,high,low,close,dif,dea,macd,ma5,ma10,ma20,ma60,ma120)
     """
     if idx > len(_df):
         return None
@@ -110,38 +114,15 @@ def get_params(_df, idx):
 
 def collect_data_by_json(_data):
     _df = pd.read_json(_data)
-    (dif, dea, macd) = talib.MACD(_df["close"],
-                                  fastperiod=short_win,
-                                  slowperiod=long_win,
-                                  signalperiod=macd_win)
-    ma5 = np.around(talib.SMA(_df["close"], timeperiod=5), 2)
-    ma10 = np.around(talib.SMA(_df["close"], timeperiod=10), 2)
-    ma20 = np.around(talib.SMA(_df["close"], timeperiod=20), 2)
-    ma60 = np.around(talib.SMA(_df["close"], timeperiod=60), 2)
-    ma120 = np.around(talib.SMA(_df["close"], timeperiod=120), 2)
-    dif = np.around(dif, 2)
-    dea = np.around(dea, 2)
-    macd = np.around(macd, 2)
-
-    _df.insert(loc=5, column="dif", value=dif)
-    _df.insert(loc=5, column="dea", value=dea)
-    _df.insert(loc=5, column="macd", value=macd)
-    _df.insert(loc=5, column="ma5", value=ma5)
-    _df.insert(loc=5, column="ma10", value=ma10)
-    _df.insert(loc=5, column="ma20", value=ma20)
-    _df.insert(loc=5, column="ma60", value=ma60)
-    _df.insert(loc=5, column="ma120", value=ma120)
-
-    return _df
+    return collect_data_by_df(_df)
 
 
 def collect_data_by_df(_df):
     if _df is None:
         return None
-    (dif, dea, macd) = talib.MACD(_df["close"],
-                                  fastperiod=short_win,
-                                  slowperiod=long_win,
-                                  signalperiod=macd_win)
+    (dif, dea, macd) = talib.MACD(
+        _df["close"], fastperiod=short_win, slowperiod=long_win, signalperiod=macd_win
+    )
     ma5 = np.around(talib.SMA(_df["close"], timeperiod=5), 2)
     ma10 = np.around(talib.SMA(_df["close"], timeperiod=10), 2)
     ma20 = np.around(talib.SMA(_df["close"], timeperiod=20), 2)
--- python/stock_data/select_move.py
+++ python/stock_data/select_move.py
@@ -3,7 +3,7 @@ import numpy as np
 from redis import *
 from datetime import datetime as dt
 
-r = Redis(host="192.168.66.47", port=6381, decode_responses=True)
+r = Redis(host="127.0.0.1", port=6379, decode_responses=True)
 
 
 def check_avg(_symbol: str) -> bool:
@@ -13,6 +13,8 @@ def check_avg(_symbol: str) -> bool:
     cp_df = collect_data_by_df(df)
     cp_df.dropna(inplace=True, axis=0)
     cp_df.reset_index(inplace=True)
+    if len(cp_df) <= 0:
+        return False
 
     (_, _, _, _, close, _, _, _, _, _, ma20, ma60, _) = get_params(
         cp_df, len(cp_df) - 1
@@ -37,6 +39,17 @@ class Stock(object):
         return format("\t%s %s %f\n" % (self.symbol, self.name, self.price))
 
 
+def calc(symbol: str, price: float, code: str):
+    if symbol.find("ST") == -1 and symbol.find("退市") == -1 and price >= 5.0:
+        code = str(code)
+        # print(simple[idx])
+        prefix = code[0:3]
+        if prefix in ("000", "300", "600") and check_avg(code) is True:
+            print("run :%s" % code)
+            return 1
+    return np.nan
+
+
 if __name__ == "__main__":
     key = format("xstp_%s" % (str(dt.today()).split(" ")[0]))
     data = r.get(key)
@@ -46,32 +59,7 @@ if __name__ == "__main__":
         r.set(key, v.to_json())
     else:
         v = pd.read_json(data)
-    simple = v["股票简称"]
-    last_prices = v["最新价"]
-    txt = ""
-    industry_list = []
-    symbol_list = []
-    code_list = []
-    price_list = []
-    for idx in range(len(simple)):
-        last_price = last_prices[idx]
-        symbol = str(simple[idx])
-        if symbol.find("ST") == -1 and symbol.find("退市") == -1 and last_price >= 5:
-            # print(simple[idx])
-            code = str(v["股票代码"][idx])
-            prefix = code[0:3]
-            if prefix in ("000", "300", "600") and check_avg(code) is True:
-                industry_list.append(get_industry(code))
-                symbol_list.append(symbol)
-                code_list.append(code)
-                price_list.append(last_price)
-
-    df = pd.DataFrame(
-        data={
-            "industry": industry_list,
-            "symbol": symbol_list,
-            "code": code_list,
-            "price": price_list,
-        }
-    )
-    df.to_excel("today.xlsx")
+    v["buy"] = v.apply(lambda x: calc(x["股票简称"], x["最新价"], x["股票代码"]), axis=1)
+    v["industry"] = v.apply(lambda x: get_industry(x["股票代码"]), axis=1)
+    v.dropna(inplace=True, axis=0)
+    v.to_excel("today.xlsx", index=False)
