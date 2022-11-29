2ee0400245cab8d4b67c7025b7084ba87340e320
NorseLZJ <52354783+NorseLZJ@users.noreply.github.com>
2022-11-29 10:32:53 +0800

    Update install_ngx.sh
    增加ubuntu需要安装的库

--- shell/install_ngx.sh
+++ shell/install_ngx.sh
@@ -2,6 +2,12 @@
 # centos7.9 install nginx 
 
 
+r=$(which apt)
+if [[ $? == 0 ]];then
+        # ubuntu 
+        apt-get install gcc g++ make libpcre3 libpcre3-dev zlib1g zlib1g-dev libssl-dev -y
+fi
+
 yum install -y pcre zlib openssl  
 yum install -y pcre-devel openssl-devel zlib-devel
 
