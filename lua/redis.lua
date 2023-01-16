ff0a38d08f15918fe40be5886145bef3e10751ce
NorseLZJ <52354783+NorseLZJ@users.noreply.github.com>
2023-01-16 14:12:42 +0800

    Update redis.lua

--- lua/redis.lua
+++ lua/redis.lua
@@ -1,41 +1,39 @@
-function __split(str, reps)
-    local r, len = {}, 0
-    if str == nil then
-        return nil
+if ARGV[4] then
+    -- 不知道为什么，这里的split函数，这样写不会报错，要是写到外边（最上边），往下调用，会出错
+    local function split(str, reps)
+        local r, len = {}, 0
+        if str == nil then
+            return nil
+        end
+        string.gsub(str, "[^" .. reps .. "]+", function(w)
+            len = len + 1
+            table.insert(r, w)
+        end)
+        return r, len
     end
-    string.gsub(str, "[^" .. reps .. "]+", function(w)
-        len = len + 1
-        table.insert(r, w)
-    end)
-    return r, len
-end
-local found = flase
-if ARGV[4] then -- derivename 非空
     local members = redis.call("ZRANGEBYSCORE", KEYS[1], "-inf", "+inf", "limit", 0, -1)
     local derivename = tostring(ARGV[4])
     for i, v in pairs(members) do
-        a, l = __split(v, ",")
+        local a, l = split(v, ",")
         if l >= 2 then
-            if to_string(a[2]) == derivename then
+            if a[2] == derivename then
                 redis.call("ZREM", KEYS[1], v)
                 redis.call("ZADD", KEYS[1], ARGV[1], ARGV[2])
-                found = true
-                break
+                return
             end
         end
     end
 end
-if (ARGV[4] and found == false) or ARGV[4] == nil then
-    redis.call("ZADD", KEYS[1], ARGV[1], ARGV[2])
-    local count = redis.call("ZCARD", KEYS[1])
-    local platformCount = tonumber(ARGV[3])
-    if count > platformCount then
-        local offset = count - platformCount
-        local limit = 0
-        local delMember = redis.call("ZRANGEBYSCORE", KEYS[1], "-inf", "+inf", "limit", limit, offset)
-        for i, v in pairs(delMember) do
-            redis.call("ZREM", KEYS[1], v)
-        end
+redis.call("ZADD", KEYS[1], ARGV[1], ARGV[2])
+local count = redis.call("ZCARD", KEYS[1])
+local platformCount = tonumber(ARGV[3])
+if count > platformCount then
+    local offset = count - platformCount
+    local limit = 0
+    local delMember = redis.call("ZRANGEBYSCORE", KEYS[1], "-inf", "+inf", "limit", limit, offset)
+    for i, v in pairs(delMember) do
+        redis.call("ZREM", KEYS[1], v)
     end
 end
 
+-- redis-cli --eval xxxx.lua
