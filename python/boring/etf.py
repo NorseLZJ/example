836ea1412d5d063d2f20280e3463dc94ed0181e5
NorseLZJ <52354783+NorseLZJ@users.noreply.github.com>
2023-04-07 11:45:22 +0800

    获取市场上的etf基金

---
+++ python/boring/etf.py
@@ -0,0 +1,18 @@
+import akshare as ak
+import os
+import pandas as pd
+
+"""
+获取ETF基金
+"""
+
+path = "ETF.csv"
+
+if __name__ == "__main__":
+    if os.path.exists(path) is False:
+        etf = ak.fund_etf_category_sina(symbol="ETF基金")
+        etf.to_csv("ETF.csv", index=False)
+
+    etf = pd.read_csv(path)
+    print(etf.head(10))
+    print(etf.tail(10))
