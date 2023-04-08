029bcc6f7cb1a8d15bb0289bd55a185b35b5cf86
liuzijian <zijianliueli@gmail.com>
2023-04-08 09:14:21 +0800

    'change'

--- python/boring/calc_stock_prop.py
+++ python/boring/calc_stock_prop.py
@@ -1,4 +1,5 @@
 import akshare as ak
+import numpy as np
 
 
 def check_position(code):
@@ -8,6 +9,8 @@ def check_position(code):
         # 计算60日和20日均线
         df["ma60"] = df["close"].rolling(60).mean()
         df["ma20"] = df["close"].rolling(20).mean()
+        df["ma10"] = df["close"].rolling(10).mean()
+        df["ma5"] = df["close"].rolling(5).mean()
         # 计算EMA12和EMA26
         df["ema12"] = df["close"].ewm(span=12).mean()
         df["ema26"] = df["close"].ewm(span=26).mean()
@@ -15,6 +18,12 @@ def check_position(code):
         df["dea"] = df["dif"].ewm(span=9).mean()
         # 计算MACD
         df["macd"] = 2 * (df["dif"] - df["dea"])
+        df.drop(inplace=True, columns=["volume", "outstanding_share", "turnover", "ema12", "ema26"])
+        column = ["ma60", "ma20", "ma10", "ma5", "dif", "dea", "macd"]
+        df[column] = np.round(df[column].astype(float), 3)
+
+        print(df.tail(10))
+        df.to_csv("temp.csv")
         # 获取最新一天的数据
         latest = df.iloc[-1]
         # 获取当前价格，60日均线和20日均线的值
@@ -34,7 +43,7 @@ def check_position(code):
 
 
 if __name__ == "__main__":
-    codes = ["300449", "300390", "002424", "601611"]
+    codes = ["601766"]
     for code in codes:
         result = check_position(code)
         print(f"股票{code}当前价格是否在60日和20日均线的中间位置：{result}")
